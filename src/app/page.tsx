2c1d3c400dbf4ee2f1a1589ae9383d60dda35184
omondisteven <omondisteven@gmail.com>
2025-05-17 18:40:49 +0300

    Fixiung build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 // /src/app/page.tsx
+import { Suspense } from 'react';
 import PaymentForm from "@/components/PaymentForm";
- 
+
 export default function Home() {
   return (
     <section className="bg-gray-100 max-w-400 h-screen flex justify-center items-center">
-      <PaymentForm />
+      <Suspense fallback={<div>Loading payment form...</div>}>
+        <PaymentForm />
+      </Suspense>
     </section>
   );
 }
